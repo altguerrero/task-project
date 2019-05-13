7a1a816a9efd72cec67f21c287c8d724d40f1386
José Guerrero <josealberto.guerreroayala@gmail.com>
2019-05-13 14:10:43 -0500

    Mini libreria para consultas ajax integrada

---
+++ src/js/modules/ajax.js
@@ -0,0 +1,12 @@
+export const ajax = request => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(request.method, request.url, true);
+    xhr.addEventListener("load", e => {
+      resolve(e.target);
+    });
+    // xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
+    xhr.send(request.data);
+  });
+};
+
